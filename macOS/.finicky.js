e2c7058622b3e77d93f286da3993f9d3f7376337
Ram Iyer <git@aprilfriday.in>
2023-06-30 10:46:26 +0530

    Fix Finicky opening the default browser for everything

--- macOS/.finicky.js
+++ macOS/.finicky.js
@@ -3,51 +3,44 @@ module.exports = {
     handlers: [
       {
         match: [
-          /^https?:\/\/(.*\.)?google\.\w{2,}\/.*$/,
-          /^https?:\/\/(.*\.)?gmail\.com\/.*$/,
-          /^https?:\/\/(.*\.)?youtube\.com\/.*$/,
-          /^https?:\/\/(.*\.)?youtu\.be\/.*$/,
-          /^https?:\/\/(.*\.)?android.com\/.*$/,
-          /^https?:\/\/(.*\.)?blogger\.com\/.*$/,
-          /^https?:\/\/(.*\.)?feedburner\.com\/.*$/,
-          /^https?:\/\/(.*\.)?google(analytics|-analytics|drive|earth|maps|mail|apps|apis|bot|commerce|syndication|usercontent)\.com\/.*$/
+          /^https:\/\/.*\.?google\.(com|in)\/?.*$/
         ],
         browser: "Google Chrome"
       },
       {
         match: [
-          /^https?:\/\/(.*\.)?amazon\.(com|in|co\.uk|ca|es|fr|de|it|co\.jp)\/.*$/,
-          /^https?:\/\/(.*\.)?primevideo\.com\/.*$/
+          /^https?:\/\/.*\.?amazon\.(com|in|co\.uk|ca|es|fr|de|it|co\.jp)\/?.*$/,
+          /^https?:\/\/.*\.?primevideo\.com\/?.*$/
         ],
         browser: "Vivaldi"
       },
       {
         match: [
-          /^https?:\/\/(.*\.)?facebook(careers|-dns|enterprise|-hardware|mail)?\.(com|net|co|com\.au|com\.mx|it)\/.*$/,
-          /^https?:\/\/(.*\.)?instagram\.com\/.*$/,
-          /^https?:\/\/(.*\.)?messenger\.com\/.*$/,
-          /^https?:\/\/(.*\.)?accountkit\.com\/.*$/,
-          /^https?:\/\/(.*\.)?fb\.(com|audio|gg|me|watch)\/.*$/,
-          /^https?:\/\/(.*\.)?fb(cdn|e2e)\.com\/.*$/,
-          /^https?:\/\/(.*\.)?fbinfra\.net\/.*$/,
-          /^https?:\/\/(.*\.)?fbwat\.ch\/.*$/,
+          /^https?:\/\/.*\.?facebook(careers|-dns|enterprise|-hardware|mail)?\.(com|net|co|com\.au|com\.mx|it)\/?.*$/,
+          /^https?:\/\/.*\.?instagram\.com\/?.*$/,
+          /^https?:\/\/.*\.?messenger\.com\/?.*$/,
+          /^https?:\/\/.*\.?accountkit\.com\/?.*$/,
+          /^https?:\/\/.*\.?fb\.(com|audio|gg|me|watch)\/?.*$/,
+          /^https?:\/\/.*\.?fb(cdn|e2e)\.com\/?.*$/,
+          /^https?:\/\/.*\.?fbinfra\.net\/?.*$/,
+          /^https?:\/\/.*\.?fbwat\.ch\/?.*$/,
         ],
         browser: "Min"
       },
       {
         match: [
-          /^https?:\/\/(.*\.)?microsoft\.com\/.*$/,
-          /^https?:\/\/(.*\.)?office\.com\/.*$/,
-          /^https?:\/\/(.*\.)?live\.com\/.*$/,
-          /^https?:\/\/(.*\.)?onedrive\.com\/.*$/,
-          /^https?:\/\/(.*\.)?outlook\.com\/.*$/,
+          /^https?:\/\/.*\.?microsoft\.com\/?.*$/,
+          /^https?:\/\/.*\.?office\.com\/?.*$/,
+          /^https?:\/\/.*\.?live\.com\/?.*$/,
+          /^https?:\/\/.*\.?onedrive\.com\/?.*$/,
+          /^https?:\/\/.*\.?outlook\.com\/?.*$/,
         ],
         browser: "Microsoft Edge"
       },
       {
         match: [
-          /^https?:\/\/(.*\.)?apple\.com\/.*$/,
-          /^https?:\/\/(.*\.)?icloud\.com\/.*$/
+          /^https?:\/\/.*\.?apple\.com\/?.*$/,
+          /^https?:\/\/.*\.?icloud\.com\/?.*$/
         ],
         browser: "Safari"
       }
